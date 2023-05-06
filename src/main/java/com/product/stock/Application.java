a6795063f7f21c956fd03e2e2161276405aea930
Leonardo Dantas <leonardo1317@yahoo.com.br>
2023-05-06 17:10:46 -0300

    Padronizando o projeto

--- src/main/java/com/product/stock/Application.java
+++ src/main/java/com/product/stock/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductStockApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductStockApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
