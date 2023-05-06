fd4658f9faf41b2abcdfb6498551eed7e943d04b
Leonardo Dantas <leonardo1317@yahoo.com.br>
2023-05-06 17:10:08 -0300

    Criando o projeto

---
+++ src/main/java/com/product/stock/ProductStockApplication.java
@@ -0,0 +1,13 @@
+package com.product.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockApplication.class, args);
+	}
+
+}
