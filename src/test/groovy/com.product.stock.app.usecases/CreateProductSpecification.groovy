79d690f60a190b42baf8133318409af7209e304b
Leonardo Dantas <leonardo1317@yahoo.com.br>
2023-05-10 08:22:27 -0300

    Iniciando test

--- src/test/groovy/com.product.stock.app.usecases/CreateProductSpecification.groovy
+++ src/test/groovy/com.product.stock.app.usecases/CreateProductSpecification.groovy
@@ -1,4 +1,15 @@
+package com.product.stock.app.usecases
+
+import com.product.stock.app.repositories.IProductRepository
 import spock.lang.Specification
 
 class CreateProductSpecification extends Specification {
+
+    def productRepository = Mock(IProductRepository)
+
+    def createProduct = new CreateProduct(productRepository)
+
+    def "shouldCreateProduct"() {
+
+    }
 }
