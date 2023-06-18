1f0c841aa975ba967d451f30a90048d6f0a3eae5
Leonardo Dantas <leonardo1317@yahoo.com.br>
2023-06-17 21:14:48 -0300

    novo padrão para code

--- src/main/java/com/product/stock/infra/http/jsons/response/ProductResponse.java
+++ src/main/java/com/product/stock/infra/http/jsons/response/ProductResponse.java
@@ -3,6 +3,7 @@ package com.product.stock.infra.http.jsons.response;
 import com.product.stock.domain.Product;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record ProductResponse(
@@ -13,12 +14,15 @@ public record ProductResponse(
         String description,
         BigDecimal price,
         int quantity,
+        LocalDateTime create,
+        LocalDateTime update,
         List<String> details
 ) {
     public static ProductResponse from(final Product product) {
         return new ProductResponse(
                 product.id(), product.code(), product.name(),
                 product.description(), product.price(),
-                product.quantity(), product.details());
+                product.quantity(), product.create(),
+                product.update(), product.details());
     }
 }
